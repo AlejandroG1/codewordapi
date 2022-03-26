43728923bb271a3689b724836a9f8504a94ac031
AlejandroG1 <49463633+AlejandroG1@users.noreply.github.com>
2022-03-26 23:03:39 +0000

    (TEST) ESCONDER SECRET KEY

--- codewordapi/settings.py
+++ codewordapi/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from importlib.resources import path
 import os
 from pathlib import Path
+from decouple import config
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-50ifisx$vxb!%*9+1qafc%l_z!g9-q94f1^#g2=h9f@7co5v5&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast = bool)
 
 ALLOWED_HOSTS = ["*"]
 
