23479504884b8f48d1d629348bd3360f6ef4d124
CristianMeSu <cristian.mendoza.s@hotmail.com>
2022-04-03 17:23:19 -0500

    Update envioCorreo.py
    Se actualiza estructura de variables y contenido de body de correo de confirmacion, ahora tiene estructura HTML

--- api/envioCorreo.py
+++ api/envioCorreo.py
@@ -1,33 +1,39 @@
 
-
-# send_attachment.py
-# import necessary packages
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import os
 import smtplib
- 
-# create message object instance
-msg = MIMEMultipart()
- 
- 
-# setup the parameters of the message
-password = "Sistemas106"
-msg['From'] = "bellastarhotel117@gmail.com"
-msg['To'] = "cristian.mendoza.s@hotmail.com"
-msg['Subject'] = "prueba"
+import imghdr
+from email.message import EmailMessage
 
- 
- 
-# create server
-server = smtplib.SMTP('smtp.gmail.com: 587')
- 
-server.starttls()
- 
-# Login Credentials for sending the mail
-server.login(msg['From'], password)
- 
- 
-# send the message via the server.
-server.sendmail(msg['From'], msg['To'], msg.as_string())
- 
-server.quit()
+EMAIL_ADDRESS = 'bellastarhotel117@gmail.com'
+EMAIL_PASSWORD = 'Sistemas106'
+
+contacts = ['YourAddress@gmail.com', 'test@example.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'Check out Bronx as a puppy!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'cristian.mendoza.s@hotmail.com'
+
+msg.set_content('This is a plain text email')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <div id=":282" class="ii gt" jslog="20277; u014N:xr6bB; 4:W251bGwsbnVsbCxbXV0."><div id=":283" class="a3s aiL "><p>Apreciado(a) Cristian Mendoza.</p>
+<p>Esta es la confirmación de la cita para Carnival.</p>
+<p>Te estaremos esperando en Cerrada Zona Costera lote 21, manzana 91, 77727 Q.R.  Playa del Carmen, Quintana Roo el junio 6, 2021 a las 8:00 pm .</p>
+<p>Gracias por confiar en nosotros.</p>
+<p><span class="il">BellaStar</span><br>
++994306007<br>
+<a href="https://josemanuel286.github.io/Proyecto-Hotel-Bellastar/index.html" target="_blank" data-saferedirecturl="https://josemanuel286.github.io/Proyecto-Hotel-Bellastar/index.html">Hotel Bellastar</a></p><div class="yj6qo"></div><div class="adL">
+
+</div></div></div>
+    </body>
+</html>
+""", subtype='html')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
