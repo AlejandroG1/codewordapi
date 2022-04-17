c100485a38e629b82fa0424237f07b3bc72f3704
AlejandroG1 <49463633+AlejandroG1@users.noreply.github.com>
2022-04-17 18:09:26 +0000

    ODBC CORREGIDO DE 18 > 17
    Se corrigio la version de driver sql

--- codewordapi/settings.py
+++ codewordapi/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'PASSWORD': "Admin1234",
         'HOST': "codeworddb.database.windows.net",
         'PORT': "1433",
-        "OPTIONS": {"driver": "ODBC Driver 18 for SQL Server", 
+        "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server", 
         },
     },
 }
